b040b20f3f2a869d63f028892c2aa995304215e3
ALBUR <burzzum@mail.ru>
2024-08-06 22:56:38 +1200

    Add files via upload

---
+++ test.c
@@ -0,0 +1,26 @@
+#include  <stdio.h>
+#include "lib.c"
+
+int main ()
+{
+    for (size_t i = 30; i < 127; i++)
+    {
+    
+        
+    int8_t this = i ;
+    char that[] = "A";
+    printf (" VAR this %-4c \n" , this);
+    printf (" VAR this %-4d \n" , r);
+    printf ("TEXT %s", text);
+
+    //printf ("%-d" , *that);
+
+        
+    
+    }
+    
+
+
+    return 0; 
+
+}
